bc6c6d4d3e9741d479600b2f0206bba3f3680cab
Jake Wymer <jakewymer@att.net>
2018-06-24 20:09:52 -0500

    add scroll on nav click

---
+++ public/js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  const goToByScroll = (id) => {
+    $('html,body').animate({
+      scrollTop: $("#"+id).offset().top},
+      'slow');
+  }
+
+  $("nav > ul > li > a").click(function(e) { 
+    // Prevent a page reload when a link is pressed
+    e.preventDefault(); 
+      // Call the scroll function
+    goToByScroll(this.id.slice(0, -4));           
+  });
+});
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -82,6 +82,6 @@
   src="https://code.jquery.com/jquery-3.3.1.min.js"
   integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
   crossorigin="anonymous"></script>
-  <script src="index.js"></script>
+  <script src="{{ asset('/js/index.js') }}"></script>
 </body>
 </html>
