15386d63a100e1bd790fbb79ee34ad19ae160be2
Jake Wymer <jakeandrewwymer@gmail.com>
2018-08-05 18:05:03 -0500

    replace styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,91 +14,148 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
     <title>Jake Wymer | Software Engineer</title>
+
 </head>
 <body>
-    <header>
-        <h1>Jake Wymer</h1>
-        <nav>
-            <ul>
-                <li><a id="about-nav"href="">About</a></li>
-                <li><a id="projects-nav" href="">Projects</a></li>
-                <li><a href="/resume">Resume</a></li>
-            </ul>
-        </nav>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav">
+      <div class="container">
+        <p class="navbar-brand">Jake Wymer</p>
+        <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Portfolio</a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/resume">Resume</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead bg-primary text-white text-center">
+      <div class="container">
+        <img class="img-fluid mb-5 d-block mx-auto profile-img" src="{{asset('images/'). '/' . $me->image}}" alt="Picture of Jake Wymer">
+        <h1 class="text-uppercase mb-0">Jake Wymer</h1>
+        <hr class="star-light">
+        <h2 class="font-weight-light mb-0">{{$me->tagline}}</h2>
+      </div>
     </header>
-    <section class="hero">
-        <img src="{{asset('images/'). '/' . $me->image}}" alt="Picture of Jake Wymer">
-        <h2>Software Engineer</h2>
-        <hr />
-        <div class="tagline">
-            <h3>{{$me->tagline}}</h3>
-        </div>
-    </section>
-    <section id="about">
-        <div class="section-title blue">
-            <i class="far fa-user fa-5x" aria-hidden="true"></i>
-            <h4>About Me</h4>
-        </div>
-        <div class="about-text-wrapper">
-                <div class="about-text">
-                    <p>
-                        {{$me->par_one}}
-                    </p>
-                </div>
-                <div class="about-text">
-                    <p>
-                        {{$me->par_two}}
-                    </p>
-                </div>
-            </div>
-    </section>
-    <section id="projects">
-        <div class="section-title">
-            <i class="far fa-keyboard fa-5x" aria-hidden="true"></i>
-            <h4>Projects</h4>
-        </div>
-        <div class="projects-wrapper">
-        @foreach ($projects as $project)
-            <a href="{{$project->github}}" target="blank">
-                <div class="project">
-                    <img src="{{asset('images/'). '/' . $project->image}}" alt="">
-                    <div class="overlay">
-                        <p>{{$project->name}}</p>
+
+    <!-- Portfolio Grid Section -->
+    <section class="portfolio" id="portfolio">
+      <div class="container">
+        <h2 class="text-center text-uppercase text-secondary mb-0">Portfolio</h2>
+        <hr class="star-dark mb-5">
+        <div class="row">
+            @foreach ($projects as $project)
+                <div class="col-md-6 col-lg-4">
+                    <a class="portfolio-item d-block mx-auto" href="{{$project->github}}" target="blank">
+                    <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
+                        <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                            <p>{{$project->name}}</p>
+                        </div>
                     </div>
+                    <img class="img-fluid" src="{{asset('images/'). '/' . $project->image}}" alt="">
+                    </a>
                 </div>
-            </a>
-        @endforeach
+            @endforeach
         </div>
+      </div>
     </section>
-    <!-- <section id="contact">
-        <div class="section-title">
-            <i class="far fa-envelope-open fa-5x" aria-hidden="true"></i>
-            <h4>Contact</h4>
+
+     <!-- About Section -->
+     <section class="bg-primary text-white mb-0" id="about">
+      <div class="container">
+        <h2 class="text-center text-uppercase text-white">About</h2>
+        <hr class="star-light mb-5">
+        <div class="row">
+          <div class="col-lg-12">
+            <p class="lead">{{$me->par_one}}</p>
+          </div>
         </div>
-        <form method="post" action="/contact">
-            {{ csrf_field() }}
-            <label for="email">Email</label>
-            <input id="email-input" type="email" name="email" hint="Email" />
-            <label for="body">Message</label>
-            <textarea rows="4" cols="50" id="message" name="body" hint="Message"></textarea>
-            <button type="submit">Send</button>
-        </form>
-    </section> -->
-    <footer>
-        <p>jakesnextjob@gmail.com</p>
-        <div class="social">
-            <a href="https://github.com/JakeWymer" target="blank"><i class="fab fa-github-square fa-3x"></i></a>
-            <a href="https://www.linkedin.com/in/jake-wymer/" target="blank"><i class="fab fa-linkedin fa-3x"></i></a>
-            <a href="https://www.facebook.com/jakesnextjob" target="blank"><i class="fab fa-facebook-square fa-3x"></i></a>
+        <div class="row">
+            <div class="col-lg-12">
+                <p class="lead">{{$me->par_two}}</p>
+            </div>
         </div>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="footer text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 mb-5 mb-lg-0">
+            <h4 class="text-uppercase mb-4">Email</h4>
+            <p class="lead mb-0">
+                jakesnextjob@gmail.com
+            </p>
+          </div>
+          <div class="col-md-6 mb-5 mb-lg-0">
+            <h4 class="text-uppercase mb-4">Connect with me</h4>
+            <ul class="list-inline mb-0">
+              <li class="list-inline-item">
+                <a class="btn-social" href="https://www.facebook.com/jakesnextjob">
+                  <i class="fab fa-facebook fa-2x"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-social" href="https://github.com/JakeWymer">
+                  <i class="fab fa-github-square fa-2x"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-social" href="https://www.linkedin.com/in/jake-wymer/">
+                  <i class="fab fa-linkedin fa-2x"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </footer>
+
+    <div class="copyright py-4 text-center text-white">
+      <div class="container">
+        <small>Copyright &copy; Jake Wymer 2018</small>
+      </div>
+    </div>
+
     <script
-  src="https://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous"></script>
+    src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+  
+    <script
+    src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+    integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+    crossorigin="anonymous"></script>
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
   <script src="{{ asset('/js/index.js') }}"></script>
 </body>
 </html>
